1c6337f1ff10d25d192650ac32d26b380c062b51
GorginZ <georgia.z.leng@gmail.com>
2020-07-06 21:57:06 +1000

    linked with rails

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-  return (
-   <hi>Helloooo World</hi>
- )
+class App extends React.Component {
+async componentDidMount() {
+  const response = await fetch("http://localhost:3000/posts")
+  const posts = await response.json()
+console.log(posts)
+}
+
+render() {
+  return(
+    <h1>Hello world app</h1>
+  );
+}
 } 
 
 export default App;
